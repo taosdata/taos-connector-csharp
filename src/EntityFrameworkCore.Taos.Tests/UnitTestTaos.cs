962dc76d4fd6e87302badb4cfea09ab18c533acf
MysticBoy <mysticboy@live.com>
2022-05-05 17:24:55 +0800

    通过docker进行测试， 并加入 动态库

--- src/EntityFrameworkCore.Taos.Tests/UnitTestTaos.cs
+++ src/EntityFrameworkCore.Taos.Tests/UnitTestTaos.cs
@@ -3,9 +3,12 @@ using Ductus.FluentDocker.Services;
 using Ductus.FluentDocker.Services.Extensions;
 using IoTSharp.Data.Taos;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Data.Common;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace EntityFrameworkCore.Taos.Tests
 {
@@ -26,11 +29,23 @@ namespace EntityFrameworkCore.Taos.Tests
             var _docker = hosts.FirstOrDefault(x => x.IsNative) ?? hosts.FirstOrDefault(x => x.Name == "default");
             container = _docker.Create("tdengine/tdengine:2.4.0.12", true, new ContainerCreateParams
             {
-                PortMappings = new[] { "6030:6030", "6035:6035", "6030:6030/udp", "6035:6035/udp" },
+                PortMappings = new[] { "6030:6030", "6035:6035", "6041:6041", "6030:6030/udp", "6035:6035/udp" },
                 Hostname = System.Net.Dns.GetHostName()
             });
             container.Start();
-            container.WaitForPort("6030/tcp");
+            var f = container.GetRunningProcesses().Rows.ToList();
+            container.WaitForProcess("taosd -c /tmp/taos");
+            container.WaitForProcess("taosadapter");
+            container.WaitForHttp($"http://{System.Net.Dns.GetHostName()}:6041/rest/login/root/taosdata",(int)TimeSpan.FromSeconds(60).TotalMilliseconds,(Ductus.FluentDocker.Common.RequestResponse response, int stat)=>
+            {
+                var jo= JObject.Parse(response.Body);
+                int result = stat;
+                if (  jo.TryGetValue("code",out  JToken?  code))
+                {
+                    result = (int)(code?.Value<int>());
+                }
+                return result;
+            });
             builder = new TaosConnectionStringBuilder()
             {
                 DataSource = System.Net.Dns.GetHostName(),
@@ -39,8 +54,30 @@ namespace EntityFrameworkCore.Taos.Tests
                 Password = "taosdata",
                 Port = 6030
             };
+            NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), DllImportResolver);
         }
+        private static IntPtr DllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+        {
+            if (libraryName == "taos")
+            {
+                // On systems with AVX2 support, load a different library.
+                if (Environment.OSVersion.Platform== PlatformID.Win32NT  && Environment.Is64BitProcess)
+                {
+                    return NativeLibrary.Load("taos_win_x64.dll");
+                }
+                else if (Environment.OSVersion.Platform == PlatformID.Win32NT && !Environment.Is64BitProcess)
+                {
+                    return NativeLibrary.Load("taos_win_x86.dll");
+                }
+                else if (Environment.OSVersion.Platform == PlatformID.Unix && Environment.Is64BitProcess)
+                {
+                    return NativeLibrary.Load("libtaos_linux_x64.so");
+                }
+            }
 
+            // Otherwise, fallback to default import resolver.
+            return IntPtr.Zero;
+        }
         [TestCleanup]
         public void Clianup()
         {
@@ -55,6 +92,8 @@ namespace EntityFrameworkCore.Taos.Tests
             using (var connection = new TaosConnection(builder.ConnectionString))
             {
                 connection.Open();
+                connection.CreateCommand($"create database {database};").ExecuteNonQuery();
+                connection.ChangeDatabase(database);
                 string[] lines = {
                     "meters,location=Beijing.Haidian,groupid=2 current=11.8,voltage=221,phase=0.28 1648432611249",
                     "meters,location=Beijing.Haidian,groupid=2 current=13.4,voltage=223,phase=0.29 1648432611250",
