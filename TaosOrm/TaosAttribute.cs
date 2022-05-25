1b90cd201a33aa52125416ab2e085a5efdb5c640
pengxiwei <pengxiwei@DESKTOP-O3FOHAC>
2022-05-25 09:07:47 +0800

    封装Taos库的ORM，持续更新

---
+++ TaosOrm/TaosAttribute.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaosOrm
+{
+    [Serializable]
+    public class TaosAttribute : Attribute
+    {
+
+        /// <summary>
+        /// 超级表
+        /// </summary>
+        public string SuperTableName { get; }
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="superTableName">超级表名</param>     
+        public TaosAttribute(string superTableName)
+        {
+            SuperTableName = superTableName;
+        }
+    }
+    [Serializable]
+    public class TaosColumnAttribute : Attribute
+    {
+        /// <summary>
+        /// 列名字
+        /// </summary>
+        public string ColumnName { get; set; }
+        /// <summary>
+        /// 列类型
+        /// </summary>
+        public TaosDataType ColumnType { get; set; }
+        public string ColumnLength { get; set; }
+        /// <summary>
+        /// 是否标签
+        /// </summary>
+        public bool IsTag { get; set; } = false;
+        /// <summary>
+        /// 是否为表名
+        /// </summary>
+        public bool IsTableName { get; set; }
+        public TaosColumnAttribute(string columnName, TaosDataType columnType, string columnLength = null, bool isTag = false, bool isTableName = false)
+        {
+            ColumnName = columnName;
+            ColumnType = columnType;
+            ColumnLength = columnLength;
+            IsTag = isTag;
+            IsTableName = isTableName;
+        }
+    }
+}
---
+++ TaosOrm/TaosDataType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaosOrm
+{
+    /// <summary>
+    /// 涛思时序数据库10种数据类型
+    /// https://www.bookstack.cn/read/TDengin-2.0-zh/spilt.1.33f53af4c5509954.md
+    /// </summary>
+    public enum TaosDataType
+    {
+        TIMESTAMP,
+        INT,
+        BIGINT,
+        FLOAT,
+        DOUBLE,
+        BINARY,
+        SMALLINT,
+        TINYINT,
+        BOOL,
+        NCHAR
+    }
+}
