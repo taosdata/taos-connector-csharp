52a8ac63513b271483f2c87e5c7ec076d5d5ebac
MysticBoy <mysticboy@live.com>
2022-11-22 12:11:21 +0800

    sql语句解析的方式替换参数

---
+++ src/EntityFrameworkCore.Taos.Tests/StatementObjectTest.cs
@@ -0,0 +1,75 @@
+﻿using IoTSharp.Data.Taos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Taos.Tests
+{
+    [TestClass]
+    public class StatementObjectTest
+    {
+        [TestMethod]
+        public void TestExecuteBulkInsert_Json()
+        {
+            const string SQLDemo1 = "";
+            const string SQLDemo2 = @"SELECT DISTINCT tbname,point_code,pol_code,station_code,station_name/* 站点名 */
+    ,station_uid,longitude,latitude,mn,dev_code,pol_name,pol_unit,pol_id FROM st_pmcm WHERE point_code = @p1;";
+            const string SQLDemo3 = @"SELECT * FROM bytable WHERE t1 = @t1 AND t2 LIKE @t11 LIMIT @t3;";
+            const string SQLDemo4 = @"INSERT INTO t_status USING st_status (status_key) TAGS ('s1') (status_time, val) VALUES (@x1, @x2);";
+            var s1 = StatementObject.ResolveCommandText(SQLDemo1);
+            Assert.AreEqual(0, s1.Count);
+            Assert.AreEqual(string.Empty, s1.CommandText);
+            var s2 = StatementObject.ResolveCommandText(SQLDemo2);
+            Assert.AreEqual(5, s2.Count);
+            Assert.AreEqual("SELECT DISTINCT tbname,point_code,pol_code,station_code,station_name", s2[0].OriginalText);
+            Assert.AreEqual("/* 站点名 */", s2[1].OriginalText);
+            Assert.AreEqual(@"
+    ,station_uid,longitude,latitude,mn,dev_code,pol_name,pol_unit,pol_id FROM st_pmcm WHERE point_code = ", s2[2].OriginalText);
+            Assert.AreEqual("@p1", s2[3].OriginalText);
+            Assert.AreEqual(";", s2[4].OriginalText);
+            Assert.AreEqual(@"SELECT DISTINCT tbname,point_code,pol_code,station_code,station_name 
+    ,station_uid,longitude,latitude,mn,dev_code,pol_name,pol_unit,pol_id FROM st_pmcm WHERE point_code = ?;", s2.CommandText);
+            var pn2 = s2.ParameterNames;
+            Assert.AreEqual(1, pn2.Length);
+            Assert.AreEqual("@p1", pn2[0]);
+            var s3 = StatementObject.ResolveCommandText(SQLDemo3);
+            Assert.AreEqual(7, s3.Count);
+            Assert.AreEqual("SELECT * FROM bytable WHERE t1 = ", s3[0].OriginalText);
+            Assert.AreEqual("@t1", s3[1].OriginalText);
+            Assert.AreEqual(" AND t2 LIKE ", s3[2].OriginalText);
+            Assert.AreEqual("@t11", s3[3].OriginalText);
+            Assert.AreEqual(" LIMIT ", s3[4].OriginalText);
+            Assert.AreEqual("@t3", s3[5].OriginalText);
+            Assert.AreEqual(";", s3[6].OriginalText);
+            Assert.AreEqual("SELECT * FROM bytable WHERE t1 = ? AND t2 LIKE ? LIMIT ?;", s3.CommandText);
+            var pn3 = s3.ParameterNames;
+            Assert.AreEqual(3, pn3.Length);
+            Assert.AreEqual("@t1", pn3[0]);
+            Assert.AreEqual("@t11", pn3[1]);
+            Assert.AreEqual("@t3", pn3[2]);
+            var s4 = StatementObject.ResolveCommandText(SQLDemo4);
+            Assert.AreEqual(7, s4.Count);
+            Assert.AreEqual("INSERT INTO t_status USING st_status (status_key) TAGS (", s4[0].OriginalText);
+            Assert.AreEqual("'s1'", s4[1].OriginalText);
+            Assert.AreEqual(") (status_time, val) VALUES (", s4[2].OriginalText);
+            Assert.AreEqual("@x1", s4[3].OriginalText);
+            Assert.AreEqual(", ", s4[4].OriginalText);
+            Assert.AreEqual("@x2", s4[5].OriginalText);
+            Assert.AreEqual(");", s4[6].OriginalText);
+            Assert.AreEqual("INSERT INTO t_status USING st_status (status_key) TAGS ('s1') (status_time, val) VALUES (?, ?);", s4.CommandText);
+            var pn4 = s4.ParameterNames;
+            Assert.AreEqual(2, pn4.Length);
+            Assert.AreEqual("@x1", pn4[0]);
+            Assert.AreEqual("@x2", pn4[1]);
+            var s5 = StatementObject.ResolveCommandText("insert into #subtable using stable tags($t1,$t2,$t3,$t4,$t5,$t6,$t7,$t8,$t9,$t10,$t11,$t12,$t13) values (@t1,@t2,@t3,@t4,@t5,@t6,@t7,@t8,@t9,@t10,@t11,@t12,@t13,@t14)");
+            Assert.AreEqual(14, s5.ParameterNames?.Count());
+            Assert.AreEqual("#subtable", s5.SubTableName);
+            Assert.AreEqual(13, s5.TagsNames.Count());
+            Assert.AreEqual("insert into ? using stable tags(?,?,?,?,?,?,?,?,?,?,?,?,?) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", s4.CommandText);
+
+        }
+    }
+}
---
+++ src/IoTSharp.Data.Taos/StatementObject.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTSharp.Data.Taos
+{
+    enum StatementState
+    {
+        None = 0,
+        Body = 1,
+        Quotation = 2,
+        Parameter = 3,
+        ShortComment = 4,
+        Comment = 5
+    }
+
+    public abstract class StatementPart
+    {
+        protected readonly string _Text;
+        protected StatementPart(string text)
+        {
+            _Text = text;
+        }
+        public string OriginalText => _Text;
+        public virtual string GetText() => _Text;
+    }
+
+    public sealed class Body : StatementPart
+    {
+        public Body(string text) : base(text)
+        {
+        }
+    }
+
+    public sealed class Literal : StatementPart
+    {
+        public Literal(string text) : base(text)
+        {
+        }
+    }
+
+    public sealed class Comment : StatementPart
+    {
+        public Comment(string text) : base(text)
+        {
+        }
+        public override string GetText() => " ";
+    }
+
+    public sealed class Param : StatementPart
+    {
+        public Param(string text) : base(text)
+        {
+        }
+        public override string GetText() => "?";
+    }
+
+    public sealed class StatementObject : IReadOnlyCollection<StatementPart>
+    {
+        private readonly StatementPart[] _Parts;
+        private StatementObject(IEnumerable<StatementPart> parts)
+        {
+            _Parts = parts.ToArray();
+        }
+
+        public string[] ParameterNames => _Parts.OfType<Param>().Select(p => p.OriginalText).ToArray();
+
+        public string CommandText => string.Join(string.Empty, _Parts.Select(p => p.GetText()));
+
+        public int Count => _Parts.Length;
+
+        public string SubTableName { get; set; }
+        public string[] TagsNames { get; set; }
+
+        public StatementPart this[int index] => _Parts[index];
+
+        public IEnumerator<StatementPart> GetEnumerator()
+        {
+            return (_Parts as IEnumerable<StatementPart>).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _Parts.GetEnumerator();
+        }
+
+        public static StatementObject ResolveCommandText(string commandText)
+        {
+            return new StatementObject(Resolve(commandText));
+        }
+
+        private static bool TryGetChar(string statement, int pos, out char ch)
+        {
+            if (statement.Length > pos)
+            {
+                ch = statement[pos];
+                return true;
+            }
+            ch = char.MinValue;
+            return false;
+        }
+
+        private static IEnumerable<StatementPart> Resolve(string statement)
+        {
+            var status = StatementState.None;
+            var pos = 0;
+            var partStart = 0;
+            while (TryGetChar(statement, pos, out char ch))
+            {
+                if (status == StatementState.None)
+                {
+                    status = StatementState.Body;
+                }
+                if (status == StatementState.Body)
+                {
+                    // 检测 ' @ -- /*
+                    if (ch == '\'')
+                    {
+                        if (pos > partStart)
+                        {
+                            yield return new Body(statement.Substring(partStart, pos - partStart));
+                            partStart = pos;
+                        }
+                        status = StatementState.Quotation;
+                    }
+                    else if (ch == '@')
+                    {
+                        if (pos > partStart)
+                        {
+                            yield return new Body(statement.Substring(partStart, pos - partStart));
+                            partStart = pos;
+                        }
+                        status = StatementState.Parameter;
+                    }
+                    else if (ch == '-')
+                    {
+                        if (TryGetChar(statement, pos + 1, out char x))
+                        {
+                            if (x == '-')
+                            {
+                                if (pos > partStart)
+                                {
+                                    yield return new Body(statement.Substring(partStart, pos - partStart));
+                                    partStart = pos;
+                                }
+                                status = StatementState.ShortComment;
+                                partStart = pos;
+                                pos++;
+                            }
+                        }
+                    }
+                    else if (ch == '/')
+                    {
+                        if (TryGetChar(statement, pos + 1, out char x))
+                        {
+                            if (x == '*')
+                            {
+                                if (pos > partStart)
+                                {
+                                    yield return new Body(statement.Substring(partStart, pos - partStart));
+                                    partStart = pos;
+                                }
+                                status = StatementState.Comment;
+                                partStart = pos;
+                                pos++;
+                            }
+                        }
+                    }
+                }
+                else if (status == StatementState.Quotation)
+                {
+                    // 检测 ' \'
+                    if (ch == '\'')
+                    {
+                        yield return new Literal(statement.Substring(partStart, pos - partStart + 1));
+                        pos++;
+                        partStart = pos;
+                        status = StatementState.Body;
+                    }
+                    else if (ch == '\\')
+                    {
+                        if (TryGetChar(statement, pos + 1, out char x))
+                        {
+                            if (x == '\'')
+                            {
+                                pos++;
+                            }
+                        }
+                    }
+                }
+                else if (status == StatementState.Parameter)
+                {
+                    // 检测 [^a-z0-9_]
+                    if (char.IsLetter(ch) || char.IsDigit(ch) || ch == '_')
+                    {
+
+                    }
+                    else
+                    {
+                        yield return new Param(statement.Substring(partStart, pos - partStart));
+                        status = StatementState.Body;
+                        partStart = pos;
+                    }
+                }
+                else if (status == StatementState.Comment)
+                {
+                    // 检测 */
+                    if (ch == '*')
+                    {
+                        if (TryGetChar(statement, pos + 1, out char x))
+                        {
+                            if (x == '/')
+                            {
+                                yield return new Comment(statement.Substring(partStart, pos - partStart + 2));
+                                status = StatementState.Body;
+                                pos += 2;
+                                partStart = pos;
+                            }
+                        }
+                    }
+                }
+                pos++;
+            }
+            if (pos > partStart)
+            {
+                if (status == StatementState.Body)
+                {
+                    yield return new Body(statement.Substring(partStart, pos - partStart));
+                }
+                else if (status == StatementState.ShortComment)
+                {
+                    yield return new Comment(statement.Substring(partStart, pos - partStart));
+                }
+                else if (status == StatementState.Parameter)
+                {
+                    yield return new Param(statement.Substring(partStart, pos - partStart));
+                }
+                else if (status == StatementState.Quotation)
+                {
+                    throw new Exception($"字面字符串未关闭：{statement}");
+                }
+                else if (status == StatementState.Comment)
+                {
+                    throw new Exception($"内嵌式注释未关闭：{statement}");
+                }
+            }
+        }
+    }
+
+}
