d126d3349080882b364bf1bef84757cfbf05a053
MysticBoy <mysticboy@live.com>
2023-01-07 15:03:34 +0800

    删除废弃

--- src/IoTSharp.Data.Taos/Driver/taosField.cs
+++ src/IoTSharp.Data.Taos/Driver/taosField.cs
@@ -7,31 +7,6 @@ using System.Text;
 
 namespace TDengineDriver
 {
-
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4, Size = 72)]
-    public struct taosField_E
-    {
-
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 65)]
-        public byte[] _name;
-        [MarshalAs(UnmanagedType.U1, SizeConst = 1)]
-        public byte type;
-        [MarshalAs(UnmanagedType.U1, SizeConst = 1)]
-        public byte precision;
-        [MarshalAs(UnmanagedType.U1, SizeConst = 1)]
-        public byte scale;
-        [MarshalAs(UnmanagedType.U4, SizeConst = 4)]
-        public int bytes;
-#if NET5_0_OR_GREATER
-        public string Name => Encoding.UTF8.GetString(_name, 0, _name.AsSpan().IndexOf((byte)0));
-#else
-        public string Name => Encoding.UTF8.GetString(_name, 0, Array.IndexOf( _name,(byte)0));
-#endif
-        public TDengineDataType DataType => (TDengineDataType)type;
-        public Type CrlType => DataType.ToCrlType();
-        public string TypeName => DataType.ToTypeName();
-        public int Size => bytes;
-    }
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4, Size = 72)]
     public struct taosField
     {
