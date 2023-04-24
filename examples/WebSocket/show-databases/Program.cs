28206e1294514ac120d8e4607e6e4acd1c160ca7
Shuduo Sang <sangshuduo@gmail.com>
2023-04-24 15:23:57 +0800

    chore: show database name over websocket connection

---
+++ examples/WebSocket/show-databases/Program.cs
@@ -0,0 +1,43 @@
+﻿using IoTSharp.Data.Taos;
+
+namespace Demo
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var builder = new TaosConnectionStringBuilder()
+            {
+                DataSource = "127.0.0.1",
+                           DataBase = null, 
+                           Username = "root",
+                           Password = "taosdata",
+                           Port = 6041,
+                           Protocol = "WebSocket"
+            };
+
+            string configDir = "/etc/taos";
+            var connection = new TaosConnection(builder.ConnectionString, configDir);
+            try
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+                Console.WriteLine("run SQL command `show databases` via WebSocket connection and outputs: \n");
+                cmd.CommandText = "show databases";
+                var res = cmd.ExecuteReader();
+                while (res.Read())
+                {
+                    Console.WriteLine(res.GetString(0));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
