fec67ad1234363de7d3360c25db789a506d21a86
MysticBoy <mysticboy@live.com>
2023-12-15 11:47:03 +0800

    升级ef依赖。

--- src/EntityFrameworkCore.Taos.Tests/SqModelTest.cs
+++ src/EntityFrameworkCore.Taos.Tests/SqModelTest.cs
@@ -1,6 +1,6 @@
-﻿using IoTSharp.Data.Taos;
+﻿using Carbunql.Analysis;
+using IoTSharp.Data.Taos;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SqModel.Analysis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,9 +20,9 @@ namespace EntityFrameworkCore.Taos.Tests
     ,station_uid,longitude,latitude,mn,dev_code,pol_name,pol_unit,pol_id FROM st_pmcm WHERE point_code = @p1;";
             const string SQLDemo3 = "SELECT * FROM bytable WHERE t1 = @t1 AND t2 LIKE @t11 LIMIT @t3;";
            // const string SQLDemo4 = "INSERT INTO t_status USING st_status (status_key) TAGS ('s1') (status_time, val) VALUES (@x1, @x2);";
-            var s1 = SqlParser.Parse(SQLDemo1);
+            var s1 = SelectQueryParser.Parse(SQLDemo1);
             Assert.AreEqual(0, ((int?)s1.Parameters?.Count).GetValueOrDefault());
-            var s2 = SqlParser.Parse(SQLDemo2);
+            var s2 = SelectQueryParser.Parse(SQLDemo2);
             Assert.AreEqual(5, ((int?)s2.Parameters?.Count).GetValueOrDefault());
 
             Assert.AreEqual("@p1", s2.Parameters?.Keys.ToArray()[0]);
@@ -30,7 +30,7 @@ namespace EntityFrameworkCore.Taos.Tests
             var pn2 = s2.Parameters;
             Assert.AreEqual(1, pn2.Count);
             Assert.AreEqual("@p1", pn2.Keys.ToArray()[0]);
-            var s3 = SqlParser.Parse(SQLDemo3);
+            var s3 = SelectQueryParser.Parse(SQLDemo3);
             Assert.AreEqual(7, s3.Parameters?.Count);
             Assert.AreEqual("@t1", s3.Parameters?.ToArray()[1].Key);
             Assert.AreEqual("@t11", s3.Parameters?.ToArray()[3].Key);
@@ -57,7 +57,8 @@ namespace EntityFrameworkCore.Taos.Tests
             //Assert.AreEqual(2, pn4.Length);
             //Assert.AreEqual("@x1", pn4[0]);
             //Assert.AreEqual("@x2", pn4[1]);
-            var s5 = SqlParser.Parse("insert into #subtable using stable tags($t1,$t2,$t3,$t4,$t5,$t6,$t7,$t8,$t9,$t10,$t11,$t12,$t13) values (@t1,@t2,@t3,@t4,@t5,@t6,@t7,@t8,@t9,@t10,@t11,@t12,@t13,@t14)");
+           
+            var s5 = SelectQueryParser.Parse("insert into #subtable using stable tags($t1,$t2,$t3,$t4,$t5,$t6,$t7,$t8,$t9,$t10,$t11,$t12,$t13) values (@t1,@t2,@t3,@t4,@t5,@t6,@t7,@t8,@t9,@t10,@t11,@t12,@t13,@t14)");
             Assert.AreEqual(14, s5.Parameters?.Count);
             Assert.AreEqual("#subtable", s5.Parameters?.ToArray()[0].Key);
             Assert.AreEqual(13, s5.Parameters?.Count(k=>k.Key.StartsWith('$')));
